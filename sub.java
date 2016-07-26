fb8501b680f692df2275713c96e0b0181c549d11
VishnuBose <vishnu.bose95@gmail.com>
2016-07-26 20:00:39 +0530

    Add files via upload

---
+++ sub.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package substact;
+
+/**
+ *
+ * @author VB
+ */
+public class sub {
+
+    /**
+     *
+     * @param num1
+     * @param num2
+     * @return
+     */
+    public sub()
+    {
+    
+    }
+    public int subtract(int num1,int num2)
+    {
+    return num1-num2;
+    }
+}
